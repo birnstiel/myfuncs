b56aa85f0c63814f775e25cb7e7887006193110c
birnstiel <birnstiel@me.com>
2020-12-14 21:39:23 +0100

    first pages test

--- README.md
+++
@@ -1,3 +0,0 @@
-# MYFUNCS
-
-This is an example package to get started with packaging of python.
--- myfuncs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .functions import mypower as fct
-__version__ = '0.0.2rc1'
-
-__all__ = ['fct']
